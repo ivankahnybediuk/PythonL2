e8e818f99734287cf752551cce970b02e71c3e87
ivankahnybediuk <56602081+ivankahnybediuk@users.noreply.github.com>
2020-12-18 15:17:30 +0300

    task3

---
+++ task3.py
@@ -0,0 +1,24 @@
+"""
+Task 3
+
+Using python as a calculator.
+
+Make a program with 2 numbers saved in separate variables a and b, then print the result for each of the following:
+
+Addition
+Subtraction
+Division
+Multiplication
+Exponent (Power)
+Modulus
+Floor division
+"""
+a = 39
+b = -8
+print('Addition: ', a + b)
+print('Subtraction: ', a - b)
+print('Division: ', a / b)
+print('Multiplication: ', a * b)
+print('Exponent (Power): ', a ** b)
+print('Modulus: ', abs(a), abs(b))
+print('Floor division: ', a // b)
