63e6d882962e762be84cdcca1450145574b57f4a
ivankahnybediuk <56602081+ivankahnybediuk@users.noreply.github.com>
2020-12-18 14:39:25 +0300

    Update task2.py

---
+++ task2.py
@@ -0,0 +1,10 @@
+"""
+Task 2
+
+Manipulate strings.
+
+Save your first and last name as separate variables, then use string concatenation to add them together with a white space in between and print a greeting.
+"""
+name = 'Ivanka'
+last_name = 'Hnybediuk'
+print('Hello, ', name, ' ', last_name, '!')
